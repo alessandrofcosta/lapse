e0c6155dfb2b9d6ad44deae0bdb2b49667ac9218
Alessandro <136349209+alessandrofcosta@users.noreply.github.com>
2026-04-06 00:08:41 -0300

    Formata lore da Ami com títulos e parágrafos

--- js/npcs/stories.js
+++ js/npcs/stories.js
@@ -114,5 +114,91 @@ window.NPC_STORIES = {
     Mesmo sem convívio constante, Caim voltou a sentir algo que havia sido destruído pelos próprios punhos:
     a sensação de pertencer a um lar.
 </p>
+`,
+    ami: `<h1>Ami Akiami, a Décima Lâmina</h1>
+
+<blockquote>
+    "Se o nosso mundo estivesse se repetindo, eu ainda admiraria a versão de cada um igualmente. Se um dia você estiver desesperado, eu segurarei sua mão com firmeza. Eu te emprestaria o meu poder em meio ao caos, pequeno Herói do Amanhã. E então, perceberá… que o que eu desejo é algo mais profundo do que uma simples paz."
+</blockquote>
+
+<p>"Olá, mundo! ♪"</p>
+
+<p>Ashurima, o berço de Ami Akiami; uma terra tão vasta em florestas, com a beleza natural esbanjada em flora. Casa de animais e lar das macieiras mais bonitas da região, Ami cresceu neste ambiente praticamente ao lado de seu irmão mais velho, Shin na vila Akiami. Dona de uma pele brilhante, olhos rosados e cabelo da mesma cor, Ami mostrou-se uma criança adorável. Apesar de Ashurima ser tranquila até então, a Vila Akiami existia para tornar isto uma tarefa difícil. Tudo que acontecia com Shin, acabava refletindo em Ami como uma consequência. Uma criança como Ami não entendia sobre aquilo. Sobre "sacrifício". Sobre "demônios". Sobre a morte em si. Essa sua pureza; a inocência infantil era a chama que mantia a fogueira de Shin acesa. Ele a defendia, brigava contra a própria família para protegê-la. Ami cresceu aprendendo como um lugar tão belo também poderia abrigar monstros. Mas, apesar disso, podia abrigar amor. O amor de seu irmão. O primeiro sentimento gentil que adentrou seu coração como uma flecha.</p>
+<p>Ami não possuía mais a permissão para sair de casa. Uma condição que bloqueou a visão das suas adoráveis macieiras; a árvore que ela mais gostava naquela floresta. Ao realizar 5 anos de idade, desenvolveu a primeira opinião controversa a seu pai. O aniversário. Seu querido pai sempre realizava eventos quando ficava mais velho. Sua mãe, também. Os moradores da vila também comemoravam. Mas ela não. Nem Shin. O que havia de errado?</p>
+<p>Noites se mostraram o inimigo da luz de seus olhos. Palavras relacionadas à morte e sacrifícios se tornavam mais frequentes; geralmente eram direcionadas à seu irmão, e agora, estavam sendo direcionadas à ela também.</p>
+<p>Em uma dessas noites, Ami estava concentrada em dormir, deitada em sua cama. Foi quando algo empurrou a janela de madeira presente em seu quarto. "Psiu!", ouviu. "Você é a Ami, certo?". Curiosa, ela foi até a janela. Ainda estava vestindo suas roupas de dormir, o tecido arrastando-se pelo chão. "Sim… sou eu", respondeu em um sussurro, preocupando-se em não fazer nenhum barulho. "O Shin falou de você para meu irmão. Ele disse que você 'tá presa", o garoto, agora visível pelos olhos de Ami, sussurrava agora. Seus cabelos eram castanhos, sua pele era morena e seus olhos eram escuros. "Meu pai não me deixa sair". "Eu sei, ele comentou disso também". Havia uma animação visível em seus olhos. "O que veio fazer aqui? Está tarde… devíamos estar dormindo", a garota indagou.</p>
+<p>"Eu vim te fazer uma pergunta… hum… a gente viu que você 'tá bem sozinha e… queríamos te convidar para nosso grupo. Somos os Heróis do Amanhã!", sua voz sussurrante falhou em um tom de excitação, e o garoto mudou sua expressão para uma de medo. "Foi mal…". Ami riu baixinho, e abriu um sorriso. "Ah, eu adoraria… mas eu preciso perguntar para o meu pai. Qual o seu nome?". "Eu sou o Sora. Sora Akiami". "Muito prazer, Sora. Eu pergunto para ele amanhã de manhã. Se eu posso participar". O garoto revirou os olhos. "Ami, não precisa falar para ele. Somos um grupo secreto. E não vamos contar para o seu pai. Vamos, agorinha! O grupo 'tá esperando". Ami arregalou os olhos (Grupo?), pensou. "M-Mas eu não posso sair sem permissão! Ainda mais que meu irmão ficaria… preocupado". Ela olhou para trás, vendo se o barulho da conversa não atrapalhava ninguém. "Relaxa, Ami. Só pega a minha mão, acho que consigo te ajudar a descer. Vambora! É hoje ou nunca". Ami relutou, mas aceitou. Precisava de algo diferente. Ficar em casa estava a deixando chateada com o passar dos dias.</p>
+<p>Agarrou a mão de Sora, e permitiu-se sair de casa, escondida. "Mas eu preciso voltar cedo, 'tá legal? E você me leva!", exclamava enquanto andavam. "Tudo bem, pode deixar."</p>
+<p>A caminhada não foi muito longa. Atravessaram caminhos de barro, árvores secas, tudo isso enquanto Sora lhe contava histórias e fofocas que sabia. Apesar da imprudência, Ami o achou engraçado, e ria vez ou outra do que falava.</p>
+<p>Chegando ao destino que Sora havia lhe preparado, Ami viu o "grupo" que haviam lhe comentado. Uma fogueira, pedras, roupas jogadas pelos cantos, comida. Tudo misturado no que parecia uma festa. No total, haviam 7 pessoas realizando aquela baderna. Quatro garotos e três garotas. Todos pareciam se divertir em uma brincadeira que envolvia correr, tudo sob um céu aberto, iluminado pela lua.</p>
+<p>"Esse é o grupo dos Heróis do Amanhã! Não somos heróis, mas nos imaginamos vencendo muitas criaturas ruins que nossos pais nos falam".</p>
+<p>"Eu…", Ami sentiu-se relutante novamente.</p>
+<p>"Está tudo de boa, Ami. Aquela dali é garotada. Vem! Vou te apresentar para eles!"</p>
+<p>Sora pegou a mão de Ami e a levou, correndo, até o grupo. Ami tropeçou diversas vezes no processo.</p>
+<p>"Gente! Eu consegui trazer ela!"</p>
+<p>A brincadeira parou gradualmente em segundos. As crianças, ainda ofegantes, viraram a atenção para a dupla que recém havia chegado.</p>
+<p>"Ah, você é a Ami! 'Pra falar a verdade, eu nem sabia sua aparência direito, você nem sai de casa", uma garota de cabelos ralos, ruivos e de aura vibrante aproximou-se.</p>
+<p>"Ela que é a irmã do último herdeiro, né?", um garoto de cabelos brancos se aproximava também.</p>
+<p>"Ugh, e o que isso te importa, Ren?" resmungava a garota loira.</p>
+<p>"É importante para a nossa historinha, Hikari!" retrucava um garoto loiro.</p>
+<p>"Calma, calma!" A estridente voz de Sora silenciou os burburinhos do grupo, "Um de cada vez. Ami, esses são meus amigos".</p>
+<p>Amy estava muito confusa. Foram informações jogadas sem piedade em sua face. Ela sorriu, torto. "Oi… prazer em conhecer".</p>
+<p>"Prazer, Ami! Eu sou o líder do grupo, Hikari, e vou te aprese-", a fala da garota loira foi cortada pelo menino loiro "Eu que sou o líder, Hikari, esqueceu?"</p>
+<p>Uma discussão se instalou no grupo, que foi resolvida após alguns minutos. O grupo se apresentou para Ami, também se apresentando como "animais".</p>
+
+<h2>Os Heróis do Amanhã</h2>
+<p>Hikari, uma garota loira, implacável e inteligente. Representada por um Cisne.</p>
+<p>Ren, um garoto de cabelos brancos, curioso e astuto. Representado por uma raposa.</p>
+<p>Natsu, uma garota ruiva, tímida, mas que falava bastante. Representada por um coelho.</p>
+<p>Aaron, um garoto de cabelos ruivos, animado e festeiro. Representado por um cachorro.</p>
+<p>Daen, um garoto quieto, de cabelos cinzentos. Representado por um corvo.</p>
+<p>Eros, um menino loiro, que gostava de implicar. Representado por um gato.</p>
+<p>Kuro, uma garota de cabelo rosa, semelhante à de Ami. Criativa. Representada por um esquilo.</p>
+<p>E claro, Sora, representado por um cavalo branco.</p>
+
+<p>"E você, Ami, será qual animal?" Hikari indagou.</p>
+<p>"Ah, eu… não sei… acho que nenhum…"</p>
+<p>"Ah, vamos lá! Deve ter algo que você tem em comum!" Opinou Aaron.</p>
+<p>"Eu… acho que não…"</p>
+<p>"Deixem ela pensar, pessoal. Não se afobem. Ami, quando a nossa aventura acabar, podemos decidir, tudo bem?"</p>
+<p>Ami demorou um tempo para se acostumar com tantas pessoas ao mesmo tempo. Mas, perto do fim da noite, ela conseguiu brincar com eles. Foi uma sensação… incrível. A primeira brincadeira que não era com o seu irmão.</p>
+<p>Os dias passaram. Ami, todos os dias, se encontrava com seu irmão de dia. Aprendeu muitas coisas com eles, como cozinhar, escalar, correr. E principalmente, aprendeu sobre um tipo de magia: runas. Sua amiga Kuro que a ensinou sobre elas; como elas brilhavam no escuro, e reproduziam poderes inimagináveis. E então, à noite, saía escondida com Sora para ir de encontro ao grupo. Juntos, escreveram histórias sobre um bando de animais que caçava um monstro gigante. Bando de animais, este, que representava o grupo. Ami, desde cedo, passou a ser a escritora da história, já que não havia nenhum animal que a representasse. Ela apenas assistia. Brincava também, claro, mas… era diferente. Sentia-se confortável sendo uma espectadora. A vida de seus amigos era tão bela. Eles pareciam tão felizes. Para Ami, isso a fazia feliz.</p>
+<p>Numa noite, seu irmão havia se atrasado. Seus pais também não estavam em casa. Com medo, Ami, curiosa por instinto, decidiu ir atrás de seu irmão. Apenas para encontrá-lo brigando com seu pai. O pai segurava uma katana, e Shin a recusava.</p>
+<p>Foi então que ouviu as palavras que deixaram-a em choque. "Prove-se. Sacrifique sua irmã". Ami escondeu-se. Sua mãe, presente na cena, chorava em relutância. As palavras se tornaram confusas, e Ami apenas viu Shin matar sua própria mãe. Após isso, sua vida tornou-se um verdadeiro inferno.</p>
+<p>Ami passou a receber tratamentos agressivos. Seu pai a agredia e abusava todas as vezes que estressava-se. Com o irmão trabalhando, ou seja, sem proteção, Ami aprendeu a aceitar aquele fardo.</p>
+<p>Sora percebia o medo em Ami, e ajudava quando o irmão não estava por perto. Os encontros com o grupo passavam a ser para cuidar de Ami e seus ferimentos. Todos ali se importavam com ela. Sentia-se segura. Sentia o amor de seus amigos.</p>
+<p>E em mais uma noite chuvosa, o caos trabalhou por hora extra. Neste dia, seu pai descobriu suas saídas à noite, e passou a segui-la para ver onde ela iria. Encontrou o grupo, o esconderijo. Tudo. E aquela raiva dentro de seu pai alastrou-se, intensificou-se. Misturou o ódio que tinha pelo seu filho, pela sua filha, pela sua vida. Tudo em apenas uma lâmina.</p>
+<p>O bando de animais não aniquilou o monstro malvado. O monstro malvado aniquilou todos eles. A espectadora não pôde fazer nada. Sangue voou pelas árvores. Sangue manchou as páginas desta história gentil. Uma história que começou com amor, e terminou com ódio.</p>
+<p>Seu pai matou todos os seus amigos, na sua frente. Ami não pôde fazer nada. Este foi seu primeiro e verdadeiro trauma. Sua cabeça parou de funcionar naquela cena. Sua visão se escureceu. O choque foi maior que sua resistência.</p>
+<p>Nesta mesma noite, Shin a resgatou. Havia prometido-a que deixaria este inferno de lugar. Ami não sabia disso. Ela apenas acordou em outro lugar, outra cidade. Como se acordasse de um sonho. Ami chorou. Estava sozinha.</p>
+
+<h2>Novo Começo</h2>
+<p>Caim Vorthal, um demônio, lhe acolheu em Akiami. Levou-a para casa. Ami estava em choque, nem reagia direito. Mas Caim era tão gentil e paciente que, com o tempo, o brilho nos olhos de Ami passou a ser restabelecido. Ela voltou a se alimentar. Voltou a caminhar. Voltou a falar. Tudo isso gradualmente, com a ajuda de Caim.</p>
+<p>Ami a considerou como um pai neste tempo. Seus 4 anos de crescimento foram graças à ele. Com 12 anos, Ami já saboreava a vida novamente. Suas memórias ficaram no passado, e seus amigos se tornaram uma representação de força e esperança. Ami carrega o livro manchado consigo até os dias de hoje. Lá, passou a escrever novas páginas. Novos animais para sua história. É lá que também permanecem os ensinamentos de Kuro sobre as runas. Ami passou a estudá-las. Como se carregasse um desejo de sua amiga.</p>
+<p>Com o tempo, Caim a treinava. Ami já havia lhe pedido antes, mas demorou um pouco para ele aceitar. E quando aceitou, foi um treino atrás do outro; Ami, uma criança de apenas 12 anos, aprendendo a lutar. Era… incrível, para ela. Via Caim da mesma forma que via Shin. Um homem que a protegia, um novo "irmão". Um novo "pai". Uma figura inspiradora.</p>
+<p>Caim, certo dia, levou-a para um treinamento em conjunto, onde estavam presentes as famosas Lâminas. Neste treinamento, Ami conseguiu enxergar seu grupo de amigos novamente. Não de uma forma triste, mas sim, um novo futuro a ser explorado. A inspiração correu pelo seu corpo. Como uma espectadora, ela precisava escrever a história sobre eles, preencher as páginas em branco de seu livro. E então, a inspiração das suas runas se instalou. A primeira runa foi dedicada ao seu amigo, Sora. Era o que mais havia presença no livro. "Quando o Esquilo implantou as asas no Cavalo, ele percebeu que havia muitas maçãs além do macieiro de seu bairro… existiam macieiros por todo lugar! Ele voava, era um milagre!", uma passagem em seu livro que inspirou sua Runa: Pégasus. A passagem que ela mesmo havia escrito.</p>
+<p>Ao passar dos anos, conheceu melhor as outras lâminas. Zerath, a mais forte, era a mais confusa para ela. Mas Zerath via potencial nela. Então, permitiu sua ânima à garota. Ela estudou Zerath, e forjou a Runa do Monstro; o mais forte em seu livro. Ami gosta de Zerath, ela o acha engraçado. Só meio assustador às vezes.</p>
+<p>Conheceu Mercy, a filha de Zerath. Muito gentil e bela. Ami ainda tenta estudar sua ânima, mas é… tão complicado.</p>
+<p>Conheceu a Guerra, um ser de duas cabeças, animado e… chato. Ami gosta quando Guerra está de armadura. E atura Guerra quando ela está sem armadura…</p>
+<p>Estudou também sobre as outras Lâminas. Por mais que não as visse direito, existiam livros na biblioteca da cidade que falavam sobre estas Lâminas, e Caim também sabia bastante.</p>
+<p>Apesar de todas essas outras Runas, Caim foi a segunda Runa que criou. O Lobo. Feroz, protetor, "babão". Uma ideia incrível. Ami perdia-se em suas histórias, vez ou outra imaginando o demônio como um grande lobo, e ela, montada nele, subindo montanhas.</p>
+<p>Não demorou muito para a demonstração do poder de Ami ser em uma escala extremamente alta para seu conhecimento e idade. Esse poder impressionante fora visto pela Primeira Lâmina como uma Lâmina prodígio. A Décima Lâmina. Quando recebeu esse título, Ami recusou. Caim celebrou. Entretanto, Ami não se considera uma Lâmina, assim como é apenas espectadora na história.</p>
+<p>Agora, com 14 anos, Ami tende a ver muito mais sobre as Lâminas, afinal, sua inspiração vem delas. Em missões, encontros, reuniões, com as Lâminas… ela vê ali, a sua família que perdeu. Sua nova vida começou. Rumo à um novo mundo.</p>
+
+<h2>Uma Mensagem aos Pequenos Heróis</h2>
+<p>"Aaron, que o seu caminho como um cachorro, seja repleto de ossos deliciosos. Que você encontre o maior Osso do Mundo!"</p>
+<p>"Hikari, um dia, o cisne irá abrir suas asas à um horizonte tão azul quanto as águas que se banham. Então, chega de reclamar, e começa a voar!"</p>
+<p>"Kuro, o esquilo, guardará suas nozes para o maior Nozomonstro no mundo! Juntos, com a genialidade de Kuro, iremos derrotar nosso inimigo!"</p>
+<p>"Natsu, sua voz não é como a dos outros coelhos. Ela é tão bonita. Não deixe sua timidez esconder sua voz. Precisamos de você para o fim desta história!"</p>
+<p>"Sora, eu te guardarei nas profundezas do meu coração. Juntos, com a ajuda do Cavalo, trilhamos uma passagem para encontrarmos o mundo que esperávamos. ~♡"</p>
+<p>"Daen, assim como o corvo, você é o mais inteligente entre nós. Ache uma brecha para podermos realizar nossas incríveis estratégias!"</p>
+<p>"Eros, deixa de ser chato, vem! Temos que acabar com isso, rápido! Então, largue o seu peixe, gato e venha conosco!"</p>
+
+<h2>Uma Mensagem às Lâminas</h2>
+<p>"Zerath, assim como o Dragão, você é livre e poderoso. Nesta história, você será o líder que acaba com o grande monstro. Mas, obviamente, com a nossa ajuda!"</p>
+<p>"Mercy, você é ágil como um grande polvo, cabendo até em lugares inimagináveis! Sua velocidade é impressionante. Mas, o que mais me impressiona, é a sua habilidade de me ler tão bem. Um dia, podemos sair juntas e escrever um novo tipo de poema, é mais fácil quando se sabe o que o outro sente… que tal? ~ ♪"</p>
+<p>"Caim, assim como um Lobo, você me reuniu em sua matilha. Eu nunca vou esquecer de sua gentileza, o Grande Herói dessa história. Trilharemos o longo caminho da vida juntos. O que acha? ~ ♡"</p>
+
+<p>"Olá, Leitor! Nesta página em branco, eu e você poderemos escrever um futuro tão brilhante quanto o nosso passado. Vamos, juntos, nesta história. Pegue sua caneta! Mas antes… qual animal você seria? ♡"</p>
 `
 };
